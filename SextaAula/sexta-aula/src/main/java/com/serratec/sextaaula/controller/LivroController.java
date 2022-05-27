b83c3f3926c5d61d7de2547e892609a79c66d21f
CristovaoSilvaMK77 <Cristovaosilvamk77@gmail.com>
2022-05-26 22:22:56 -0300

    Atualiza LivroController

--- SextaAula/sexta-aula/src/main/java/com/serratec/sextaaula/controller/LivroController.java
+++ SextaAula/sexta-aula/src/main/java/com/serratec/sextaaula/controller/LivroController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,22 @@ public class LivroController {
 		header.add("lista-livro", "Segue a lista de livros.");
 		return new ResponseEntity<List<Livro>>(service.getAll(parametro), header, HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getonebyId(@PathVariable Integer id) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("livro-unico", "Segue o livro requisitado.");
+		return new ResponseEntity<>(service.findbyID(id), header, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> post(@RequestBody Livro livro) {
+		service.post(livro);
+		HttpHeaders header = new HttpHeaders();
+		header.add("adicao-livro", "O livro foi adcionado.");
+		return new ResponseEntity<>(header, HttpStatus.CREATED);
+	}
+	
+	
 
 }
